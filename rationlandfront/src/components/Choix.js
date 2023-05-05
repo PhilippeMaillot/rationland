6e06372f6b5c9936facccb1598c8bd2b92e71675
Tristandac28 <tristandac28@gmail.com>
2023-05-05 16:03:01 +0200

    ta les crampté ?

--- rationlandfront/src/components/Choix.js
+++ rationlandfront/src/components/Choix.js
@@ -1,35 +1,46 @@
 import { useState, useEffect } from 'react';
-import '../styles/Choix.css'
+import '../styles/Choix.css';
+
 export default function Choix() {
   const [question, setQuestion] = useState('');
-  const [imageURL, setImageURL] = useState('');
+  const [imagePath, setImagePath] = useState('');
   const [images, setImages] = useState([]);
+  const [basePath, setBasePath] = useState('img/'); // ajout de l'état basePath
 
   useEffect(() => {
     async function fetchImages() {
       const response = await fetch('/images');
       const data = await response.json();
-      setImages(data);
+      const imagesWithFullPath = data.map(image => {
+        return {
+          ...image,
+          path: basePath + image.imageUrl.split('/').pop() // construction du chemin complet
+        };
+      });
+      setImages(imagesWithFullPath);
     }
     fetchImages();
-  }, []);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // ici vous pouvez envoyer la réponse de l'utilisateur à votre API pour la vérification
-  }
+  }, [basePath]); // écoute de l'état basePath
 
   const handleNewQuestion = () => {
     const randomImageIndex = Math.floor(Math.random() * images.length);
     const randomImage = images[randomImageIndex];
-    setQuestion(`Quel est le titre de ce jeu vidéo ? ${randomImage.name}`);
-    setImageURL(randomImage.imageUrl);
-  }
+    setQuestion(`Quel est le titre de ce jeu vidéo ?`);
+    setImagePath(randomImage.path);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const reponseUtilisateur = e.target.reponse.value.toLowerCase();
+    const reponseAttendue = imagePath.split('.')[0].toLowerCase();
+    const estCorrect = reponseUtilisateur === reponseAttendue;
+    setQuestion(`Quel est le titre de ce jeu vidéo ? La réponse était "${reponseAttendue}". Votre réponse est ${estCorrect ? 'correcte' : 'incorrecte'}.`);
+  };
 
   return (
     <div>
       <h1>{question}</h1>
-      <img src={imageURL} alt="Image de blind test" />
+      {imagePath && <img src={imagePath} alt="Image de blind test" />} {/* utilisation de imagePath */}
       <form onSubmit={handleSubmit}>
         <label htmlFor="reponse">Réponse :</label>
         <input type="text" id="reponse" name="reponse" />
