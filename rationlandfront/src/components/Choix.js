286741a290a15bf3894893b6dba995f85884092c
Tristandac28 <tristandac28@gmail.com>
2023-05-05 15:31:37 +0200

    haha

--- rationlandfront/src/components/Choix.js
+++ rationlandfront/src/components/Choix.js
@@ -1,8 +1,18 @@
-import { useState } from 'react';
-
-export default function Choix(){
+import { useState, useEffect } from 'react';
+import '../styles/Choix.css'
+export default function Choix() {
   const [question, setQuestion] = useState('');
   const [imageURL, setImageURL] = useState('');
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    async function fetchImages() {
+      const response = await fetch('/images');
+      const data = await response.json();
+      setImages(data);
+    }
+    fetchImages();
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -10,9 +20,10 @@ export default function Choix(){
   }
 
   const handleNewQuestion = () => {
-    // ici vous pouvez appeler votre API pour récupérer une nouvelle question et une nouvelle image
-    setQuestion('Quel est le titre de ce jeux vidéo ?');
-    setImageURL('https://votreapi.com/image/blindtest.jpg');
+    const randomImageIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomImageIndex];
+    setQuestion(`Quel est le titre de ce jeu vidéo ? ${randomImage.name}`);
+    setImageURL(randomImage.imageUrl);
   }
 
   return (
@@ -26,5 +37,5 @@ export default function Choix(){
       </form>
       <button onClick={handleNewQuestion}>Nouvelle question</button>
     </div>
-  )
+  );
 }
