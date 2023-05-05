bd09f59276df86bfd36e46f6df5d0f6af9d75ff5
Vonnebul <vonnebul@gmail.com>
2023-05-05 17:45:14 +0200

    node post add into test.json

--- serveur/server.js
+++ serveur/server.js
@@ -3,6 +3,9 @@ const app = express() // variable utilisant la librairie express
 const jeux = require('./test.json')
 var cors = require('cors')
 const fs = require('fs');
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
 
 
 app.use(cors())
@@ -21,6 +24,35 @@ app.get('/jeux/:id', (req,res) => {
 app.get('/nombrejeux', (req,res)=>{
   res.status(200).json(jeux.length)
 
+})
+app.post('/addJeux', (req,res)=>{
+  console.log(req.body)
+  const newJeux = {  
+    "id": req.body.id,
+    "name": req.body.name,
+    "imageUrl": req.body.imageUrl
+};
+
+  fs.readFile('./test.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Erreur serveur 1');
+    }
+
+    const jeux = JSON.parse(data);
+
+    jeux.push(newJeux);
+
+    const json = JSON.stringify(jeux, null, 2);
+    fs.writeFile('./test.json', json, (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Erreur serveur 2');
+      }
+
+      res.send('Jeux ajouté avec succès');
+    });
+  });
 })
 
 app.listen(8000, () => {
