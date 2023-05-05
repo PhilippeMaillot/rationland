96cfc1a43a54de8338413f28b18f52ef73f57675
Vonnebul <vonnebul@gmail.com>
2023-05-05 15:08:04 +0200

    modif api

--- serveur/server.js
+++ serveur/server.js
@@ -5,10 +5,16 @@ var cors = require('cors')
 app.use(cors())
 
 
-app.get('/images', (req, res) => {
+app.get('/jeux', (req, res) => {
 
-  res.json(images);
+  res.json(jeux);
 });
+
+app.get('/jeux/:id', (req,res) => {
+  const id = parseInt(req.params.id)
+  const leJeux = jeux.find(jeu => jeu.id === id)
+  res.status(200).json(leJeux)
+})
 
 app.listen(8000, () => {
   console.log('Server started on port 8000');
